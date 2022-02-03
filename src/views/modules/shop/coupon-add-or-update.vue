858b5d3ab7971da9f8dcd45946fa61ca078e3465
LiYang <email@example.com>
2022-02-03 17:15:07 +0800

    间断性提交

--- src/views/modules/shop/coupon-add-or-update.vue
+++ src/views/modules/shop/coupon-add-or-update.vue
@@ -65,8 +65,47 @@
           <i class="el-icon-plus"></i>
         </el-upload>
       </el-form-item>
-      <el-form-item prop="detail" :label="$t('coupon.couponDetail')">
+      <!--<el-form-item prop="detail" :label="$t('coupon.couponDetail')">
         <el-input v-model="dataForm.detail" :placeholder="$t('coupon.couponDetail')"></el-input>
+      </el-form-item>-->
+      <el-form-item
+        v-for="(item, index) in dataForm.detailList"
+        :key="item.key"
+        :prop="`detailList.${index}.detail`"
+        :label="index === 0 ? $t('商品属性') : ''"
+        class="resource-list">
+        <el-row>
+          <el-col :span="4">
+            <el-input v-model="item.detail_name" :placeholder="$t('属性名')">
+              <!--
+                //这里暂时不引入复合型输入框
+                <el-select  v-model="item.select"  slot="prepend" placeholder="请选择" >
+                <el-option label="上市时间" value="上市时间"></el-option>
+                <el-option label="订单号" value="订单号"></el-option>
+                <el-option label="用户电话" value="用户电话"></el-option>
+                <el-option label="餐厅名" value="餐厅名"></el-option>
+                <el-option label="订单号" value="订单号"></el-option>
+                <el-option label="用户电话" value="用户电话"></el-option>
+                <el-option label="餐厅名" value="餐厅名"></el-option>
+                <el-option label="订单号" value="订单号"></el-option>
+                <el-option label="用户电话" value="用户电话"></el-option>
+                <el-option label="餐厅名" value="餐厅名"></el-option>
+                <el-option label="订单号" value="订单号"></el-option>
+                <el-option label="用户电话" value="用户电话"></el-option>
+              </el-select>-->
+            </el-input>
+          </el-col>
+          <el-col :span="18">
+            <el-input v-model="item.detail_info" :placeholder="$t('商品属性内容')">
+            </el-input>
+          </el-col>
+          <el-col :span="2" class="text-center">
+            <el-button @click="resourceDeleteHandle(item)" size="small" type="text">{{ $t('delete') }}</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item :label="dataForm.detailList.length <= 0 ? $t('参数') : ''">
+        <el-button @click="resourceAddHandle()" class="aui-button--dashed w-percent-100">{{ $t('添加') }}</el-button>
       </el-form-item>
       <el-alert
         title="因商品的特殊性请仔细检查提交数据是否正确，一旦提交不可修改"
@@ -92,7 +131,7 @@ export default {
       dataForm: {
         id: '',
         name: '',
-        time0: null,
+        time0: [],
         time1: '',
         time2: '',
         type: '',
@@ -102,7 +141,7 @@ export default {
         amount2: '',
         pic: '',
         url: '',
-        detail: ''
+        detailList: []
       }
     }
   },
@@ -123,6 +162,8 @@ export default {
     init () {
       this.visible = true
       this.url = `${window.SITE_CONFIG['apiURL']}/oss/file/upload?token=${Cookies.get('token')}`
+      this.detailList = []
+      // this.dataForm.time0 = null
     },
     // 图片上传之前
     beforeUploadHandle (file) {
@@ -139,13 +180,25 @@ export default {
       // 将图片的访问地址填充到表单的url字段中
       this.dataForm.url = res.data.url
     },
+    // 添加商品属性
+    resourceAddHandle () {
+      this.dataForm.detailList.push({
+        key: new Date().getTime(),
+        detail_name: '',
+        detail_info: ''
+      })
+    },
+    // 删除商品属性
+    resourceDeleteHandle (resource) {
+      this.dataForm.detailList = this.dataForm.detailList.filter(item => item.key !== resource.key)
+    },
     // 表单提交
     dataFormSubmitHandle: debounce(function () {
       this.$refs['dataForm'].validate((valid) => {
         if (!valid) {
           return false
         }
-        this.$http[!this.dataForm.id ? 'post' : 'put']('/shop/coupon', this.dataForm).then(({ data: res }) => {
+        this.$http[!this.dataForm.id ? 'post' : 'put']('/shop/coupon/save', this.dataForm).then(({ data: res }) => {
           if (res.code !== 0) {
             return this.$message.error(res.msg)
           }
@@ -164,3 +217,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.resource-list {
+  .el-select .el-input__inner {
+    min-width: 110px;
+    text-align: center;
+  }
+}
+</style>
--- src/views/modules/shop/coupon.vue
+++ src/views/modules/shop/coupon.vue
@@ -3,11 +3,10 @@
     <div class="mod-sys__params">
       <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
         <el-form-item>
-          <el-button v-if="$hasPermission('sys:params:save')" type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
+          <el-button v-if="$hasPermission('shop:coupon:save')" type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
         </el-form-item>
       </el-form>
-      <el-table v-loading="dataListLoading" :data="dataList" border @selection-change="dataListSelectionChangeHandle" style="width: 100%;">
-        <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table v-if="$hasPermission('shop:coupon:view')" v-loading="dataListLoading" :data="dataList" border @selection-change="dataListSelectionChangeHandle" style="width: 100%;">
         <el-table-column prop="name" :label="$t('coupon.couponName')" header-align="center" align="center"></el-table-column>
         <el-table-column prop="type" :label="$t('coupon.couponType')" header-align="center" align="center">
           <template slot-scope="scope">
@@ -35,10 +34,31 @@
         </el-table-column>
         <el-table-column prop="date" :label="$t('coupon.coupondate')" header-align="center" align="center">
           <template slot-scope="scope">
-            <el-button v-if="$hasPermission('shop:coupon:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t('coupon.coupondate') }}</el-button>
+            <el-button v-if="$hasPermission('shop:coupon:info')" type="text" size="small" @click="shop_info(scope.row.id,scope.row.type,scope.row.memberlevel)">{{ $t('coupon.coupondate') }}</el-button>
           </template>
         </el-table-column>
       </el-table>
+      <!--显示详细信息的弹窗-->
+      <template>
+        <el-dialog  title="商品详细信息" :visible.sync="shop_infoVisible" :close-on-click-modal="false" :close-on-press-escape="false">
+          <!--商品大图-->
+          <span style="font-size: 30px;">商品大图浏览</span>
+          <img :src="src" loading="lazy" style="width: 100%;display:block;margin: 10px 0px 10px 0px;">
+          <!--商品当前状态-->
+          <span style="font-size: 30px;">商品当前状态</span>
+          <el-steps :active="active" direction="vertical" style="margin: 10px 0px 10px 0px;">
+            <el-step title="未开始" description="管理员已将优惠商品录入系统"></el-step>
+            <el-step title="进行中" :description="description"></el-step>
+            <el-step title="已结束" description="该商品优惠活动已经结束"></el-step>
+          </el-steps>
+          <!--商品详细参数-->
+          <span style="font-size: 30px;">商品详细信息</span>
+          <el-table :data="tableData" style="width:700px;margin: 10px 0px 10px 0px;">
+            <el-table-column prop="detail_name" label="商品属性" width="350px"></el-table-column>
+            <el-table-column prop="detail_info" label="属性内容" width="350px"></el-table-column>
+          </el-table>
+        </el-dialog>
+      </template>
       <el-pagination
         :current-page="page"
         :page-sizes="[10, 20, 50, 100]"
@@ -53,7 +73,6 @@
     </div>
   </el-card>
 </template>
-
 <script>
 import mixinViewModule from '@/mixins/view-module'
 import AddOrUpdate from './coupon-add-or-update'
@@ -66,9 +85,81 @@ export default {
         getDataListIsPage: true,
         deleteIsBatch: true
       },
+      shop_infoVisible: false,
+      info_url: '',
+      // 详细参数列表
+      tableData: [],
+      data: [],
+      // 商品图片地址
+      src: '',
+      // 活动当前状态
+      active: '',
+      // 步骤条数据绑定
+      description: '',
       dataForm: {
         paramCode: ''
       }
+    }
+  },
+  methods: {
+    // 商品详细
+    /* eslint-disable */
+    shop_info (id,type,memberlevel) {
+      this.shop_infoVisible = true
+      this.info_url = `${window.SITE_CONFIG['apiURL']}/shop/coupon/${id}`
+      this.$nextTick(() => {
+        this.$http.get(this.info_url).then(({ data: res }) => {
+        // 如果请求失败
+        if (res.code !== 0) {
+          this.data = []
+          return this.$message.error(res.msg)
+        }
+        // 如果请求成功
+        // 获取数据
+        this.data = res.data  
+        var pp=typeof res.data !== 'object' ? JSON.parse(res.data) : res.data;
+        // 商品图片url
+        this.src=pp.url
+        // 详细参数
+        var detail = JSON.parse(pp.detail)
+        console.log(detail)
+        this.tableData=[]
+        // 创建一个新数组
+        let arry=[]
+        for(var p1 in detail){
+          // 日志打印
+          console.log(p1)
+          console.log(detail.length)
+          console.log(detail[p1].detail_name)
+          console.log(detail[p1].detail_info)
+          // 添加元素
+          let p = {
+              detail_name: detail[p1].detail_name,
+              detail_info: detail[p1].detail_info
+          };
+          arry.push(p);
+        }
+        // 将商品品牌加入数组首部
+          let pic = {
+              detail_name: '品牌',
+              detail_info: pp.pic
+          };
+          arry.unshift(pic)
+          this.tableData=arry
+          // 步骤条数据绑定
+          this.active=memberlevel
+          // 秒杀数据绑定
+          if(type === 1){
+            this.description="秒杀开始时间："+pp.time2
+          }else{
+            // 预购数据绑定
+            var time0 = JSON.parse(pp.time0)
+            this.description="抢购开始："+time0.start_time+"     "
+                            +"结束："+time0.end_time+"     "+
+                            "公布："+pp.time1
+          }
+        })
+      })
     }
   },
   components: {
